840a0c211a8f5818a1dc5ca66b5c9949caa14f9f
Pranav <khandagalepranav1546@gmail.com>
2024-07-19 10:11:06 +0530

    Day_4

---
+++ Day_4/Valid Palindrome 2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return solve(s, i + 1, j) || solve(s, i, j - 1);
+            }
+        }
+        return true;
+    }
+
+    public static boolean solve(String s, int i, int j) {
+        while (i < j) {
+
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
