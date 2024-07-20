5e058d531592e3546cf816105120f832c0969edb
Pranav <khandagalepranav1546@gmail.com>
2024-07-20 10:34:25 +0530

    valid Number

--- Day_5/Valid Number.java
+++ Day_5/Valid Number.java
@@ -1,31 +1,34 @@
 class Solution {
     public boolean isNumber(String s) {
-        boolean numSeen = false;
+        boolean numberSeen = false;
         boolean eSeen = false;
-        boolean dotSeen = false;
+        boolean dotCheck = false;
 
-        for(int i = 0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(Character.isDigit(ch)) {
-                numSeen = true;
-            } else if(ch == '+' || ch == '-') {
-                if(i>0 && (s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E')) {
+
+            if (Character.isDigit(ch)) {
+                numberSeen = true;
+            } else if (ch == '+' || ch == '-') {
+                if (i > 0 && (s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')){
                     return false;
                 }
-            } else if(ch == '.') {
-                if(dotSeen || eSeen) {
+            } else if (ch == '.') {
+                if (dotCheck || eSeen) {
                     return false;
                 }
-                dotSeen = true;
-            } else if(ch == 'e' || ch == 'E') {
-                if(eSeen || !numSeen) {
+                dotCheck = true;
+            } else if (ch == 'e' || ch == 'E') {
+                if (eSeen || !numberSeen) {
                     return false;
                 }
                 eSeen = true;
+                numberSeen = false;
             } else {
-                return false;
+                return false; 
             }
         }
-        return numSeen;
+
+        return numberSeen;
     }
 }
