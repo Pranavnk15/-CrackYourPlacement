77305acbf87f4c8fe0d0278e15faaee5dec8f0ec
Pranav <khandagalepranav1546@gmail.com>
2024-07-26 11:57:59 +0530

    circular toue

--- Day_11/Circular Tour.java
+++ Day_11/Circular Tour.java
@@ -1,1 +1,23 @@
-Circular tour
+class Solution
+{
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(int petrol[], int distance[])
+    {
+	// Your code here	
+	int n = petrol.length;
+	int start = 0, sum = 0, diff = 0;
+	for(int i = 0; i<n; i++) {
+	    sum += petrol[i] - distance[i];
+	    if(sum < 0) {
+	        diff += sum;
+	        sum = 0;
+	        start = i+1;
+	    }
+	}
+	if(sum + diff >=0){
+	    return start;
+	}
+	return -1;
+    }
+}
